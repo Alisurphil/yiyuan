031950b565522011e8cb79bb5475a6d6f690460e
谢雪彬 <q131968850@qq.com>
2016-05-16 09:52:18 +0800

    基础

---
+++ yiyuan/yiyuan/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  yiyuan
+//
+//  Created by 谢雪彬 on 16/5/16.
+//  Copyright © 2016年 谢雪彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
